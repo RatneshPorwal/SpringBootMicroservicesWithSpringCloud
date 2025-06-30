4d84b68fba77cce126386008937a31882e21f8db
RatneshPorwal <ratneshporwal@gmail.com>
2025-06-30 11:47:29 +0530

    Basic Spring boot rest api project

---
+++ learn-spring-boot/src/main/java/com/ratnesh/spring/learn_spring_boot/LearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ratnesh.spring.learn_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
---
+++ learn-spring-boot/src/main/java/com/ratnesh/spring/learn_spring_boot/course/controller/CourseController.java
@@ -0,0 +1,24 @@
+package com.ratnesh.spring.learn_spring_boot.course.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ratnesh.spring.learn_spring_boot.course.bean.Course;
+
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> courses()
+	{
+		return Arrays.asList(
+				new Course(1,"Spring","ranga"),
+				new Course(2,"Spring Boot","john")
+				);
+	}
+
+}
