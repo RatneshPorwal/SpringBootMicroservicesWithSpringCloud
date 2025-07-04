c532eb9bc2aee8e7a9670ae726af7d3351afe3f5
RatneshPorwal <ratneshporwal@gmail.com>
2025-07-04 11:17:44 +0530

    adding basic asserts

---
+++ junit-basic/test/com/ratnesh/junit/MyAssertsTest.java
@@ -0,0 +1,25 @@
+package com.ratnesh.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyAssertsTest {
+	
+	private MyMath myMath=new MyMath();
+
+
+	@Test
+	void testCalculateSum_ThreeNumbers() {
+		assertEquals(6, myMath.calculateSum(new int[] {1,2,3}));
+		
+	}
+
+	
+	@Test
+	void testCalculateSum_NoNumbers() {
+		int result=myMath.calculateSum(new int[] {});
+		assertEquals(0, result);
+		
+	}
+}
--- junit-basic/test/com/ratnesh/junit/MyMathTest.java
+++ junit-basic/test/com/ratnesh/junit/MyMathTest.java
@@ -2,24 +2,27 @@ package com.ratnesh.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 	
-	private MyMath myMath=new MyMath();
+	private List<String> courses= new ArrayList<String>(Arrays.asList("AWS","Spring","Spring Boot"));
 
 
 	@Test
-	void testCalculateSum_ThreeNumbers() {
-		assertEquals(6, myMath.calculateSum(new int[] {1,2,3}));
+	void testAssertTrue_courseAvailability() {
+		assertTrue(courses.contains("AWS"));
 		
 	}
 
 	
 	@Test
-	void testCalculateSum_NoNumbers() {
-		int result=myMath.calculateSum(new int[] {});
-		assertEquals(0, result);
+	void testAssertFalse_courseAvailability() {
+		assertFalse(courses.contains("GCP"));
 		
 	}
 }
